4ae00d29bfa488515546e33426b1270eb677ee1a
mfarzamalam <farzualam9999@gmail.com>
2020-04-16 18:55:25 +0500

    Added update_unitmanager.php

---
+++ update_unitmanager.php
@@ -0,0 +1,31 @@
+<?php
+
+  include('common.php');
+?>
+
+<?php
+
+    $UNAME = $_POST['name1'];
+    $USOLD = $_POST['sold1'];
+    $UFLATS = $_POST['flats1'];
+    $UMAINTAIN = $_POST['monthlym1'];
+    $UCOMMENT = $_POST['comment1'];
+    $UID = $_POST['id'];
+    
+    // echo $UNAME.$USOLD.$UFLATS.$UMAINTAIN.$UID;
+
+    $query1 = "UPDATE `flats` SET `name`=$UNAME, `sold`=$USOLD, `flats`=$UFLATS, `Broker`=$UCOMMENT, `monthlym`=$UMAINTAIN, WHERE `id`='$UID' ";
+
+    $update_data = mysqli_query($connect,$query1);
+    
+  
+?>
+
+  <head>
+
+<script language="javascript">
+
+setTimeout("self.close();",500)
+
+</script> 
+</head>
