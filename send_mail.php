ae0747a71678b224113a2acc4cad369f177741b8
Usama Hussain <usamahussain400@gmail.com>
2020-05-03 02:08:16 +0500

    send mail function

--- send_mail.php
+++ send_mail.php
@@ -5,32 +5,37 @@
   require 'PHPMailer-master/src/PHPMailer.php';
   require 'PHPMailer-master/src/SMTP.php';
 
-  $mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->Mailer = "smtp";
+ 
+function send_mail($subject, $content,$recepicent ,$recepicent_name ){  
 
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->Mailer = "smtp";
 
-$mail->SMTPDebug  = 1;  
-$mail->SMTPAuth   = TRUE;
-$mail->SMTPSecure = "tls";
-$mail->Port       = 587;
-$mail->Host       = "smtp.gmail.com";
-$mail->Username   = "your-gmail@gmail.com";// Your GMAIL 
-$mail->Password   = "Password"; // GMAIL PASSWORD
-$mail->IsHTML(true);
-$mail->AddAddress("syedqasim8888@gmail.com", "Syed Qasim Ahmed"); // RECIPIENTS ADDRESS
-$mail->SetFrom("syedqasim8888@gmail.com", "Qasim"); // FROM ADDRESS
-//$mail->AddReplyTo("reply-to-email@domain", "reply-to-name"); // Reply to if any
-//$mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");  // CC 
-$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
-$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+    $mail->SMTPDebug  = 1;  
+    $mail->SMTPAuth   = TRUE;
+    $mail->SMTPSecure = "tls";
+    $mail->Port       = 587;
+    $mail->Host       = "smtp.gmail.com";
+    $mail->Username   = "your-gmail@gmail.com";// Your GMAIL 
+    $mail->Password   = "Password"; // GMAIL PASSWORD
+    $mail->IsHTML(true);
+    $mail->AddAddress($recepicent, $recepicent_name); // RECIPIENTS ADDRESS
+    $mail->SetFrom("fahad@pagespak.com", "MNM"); // FROM ADDRESS
+    //$mail->AddReplyTo("reply-to-email@domain", "reply-to-name"); // Reply to if any
+    //$mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");  // CC 
+    $mail->Subject = $subject;
+   
+    
+    $mail->MsgHTML($content); 
+    if(!$mail->Send()) {
+        var_dump($mail);
+   
+        return    "Error while sending Email.";
+    } else {
+        return      "Email sent successfully";
+    }
+     $string;   
+  }
 
-
-$mail->MsgHTML($content); 
-if(!$mail->Send()) {
-  echo "Error while sending Email.";
- // var_dump($mail);
-} else {
-  echo "Email sent successfully";
-}
 ?>
